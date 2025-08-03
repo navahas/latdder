52d02f850350f15e0e1895bd41cb13efc11d4e37
Carlos Navajas <c.navajas4@gmail.com>
2025-08-03 12:19:16 +0200

    feat: level 4 toast

--- ladder/lib.rs
+++ ladder/lib.rs
@@ -6,4 +6,5 @@ pub mod toast {
     pub mod level_01;
     pub mod level_02;
     pub mod level_03;
+    pub mod level_04;
 }
---
+++ tests/toast/level_04.rs
@@ -0,0 +1,25 @@
+use ladder::toast::level_04::*;
+
+#[cfg(test)]
+mod toast_level_04 {
+    use super::*;
+
+    #[test]
+    fn valid_eject_sequence() {
+        let _ = Toaster::<Unplugged, NoBread>::new()
+            .plug_in()
+            .insert_bread()
+            .set_timer(2)
+            .toast()
+            .eject();
+    }
+
+    // Uncommenting this should not compile:
+    // #[test]
+    // fn cannot_eject_before_toast() {
+    //     let _ = Toaster::<Unplugged, NoBread>::new()
+    //         .plug_in()
+    //         .insert_bread()
+    //         .eject();
+    // }
+}
