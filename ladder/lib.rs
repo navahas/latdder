3aa7e063598e9fcd1b1eb94eadb70c7eecce0dbc
Carlos Navajas <c.navajas4@gmail.com>
2025-08-15 22:08:19 +0200

    chore: remove unnecesary comments

--- ladder/lib.rs
+++ ladder/lib.rs
@@ -8,7 +8,6 @@ pub mod init {
 pub mod toast {
     pub mod api;
     pub mod levels {
-        // If NO higher level is enabled, compile level_01
         #[cfg(all(
             feature = "toast_level_01",
             not(feature = "toast_level_02"),
@@ -18,7 +17,6 @@ pub mod toast {
         ))]
         pub mod level_01;
 
-        // If L2 is the highest enabled, compile only level_02
         #[cfg(all(
             feature = "toast_level_02",
             not(feature = "toast_level_03"),
@@ -27,7 +25,6 @@ pub mod toast {
         ))]
         pub mod level_02;
 
-        // Same idea for higher levels when you add them:
         #[cfg(all(
             feature = "toast_level_03",
             not(feature = "toast_level_04"),
